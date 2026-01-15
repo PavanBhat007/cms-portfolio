6510e78b0a4ec06b113abaf11579077e60da998a
pavanks <pvn.24.offi@gmail.com>
2026-01-15 22:33:54 +0530

    fix: main page.js changes

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import { ArrowRight, GithubIcon, LinkedinIcon } from "lucide-react";
 import Image from "next/image";
+import { Hero } from "../../components/Hero";
+import { Projects } from "../../components/Projects";
 
 export const Portfolio = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
